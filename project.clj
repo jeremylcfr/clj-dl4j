94dd48494d874a289e70e80fc835a2598bf717f4
JeremyLC <jeremylecorguille@live.fr>
2023-03-03 17:09:19 +0100

    refactor to separate examples

--- project.clj
+++ project.clj
@@ -5,20 +5,8 @@
   :license {:name "Apache License"
             :url "https://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [org.clojure/algo.generic "0.1.3"]
                  [io.github.jeremylcfr/clj-nd4j "0.1.0-SNAPSHOT"]
                  [io.github.jeremylcfr/clj-datavec "0.1.0-SNAPSHOT"]
-                 [org.deeplearning4j/deeplearning4j-core "1.0.0-M2.1"]
-
-
-                 [org.jfree/jfreechart "1.5.4"]
-
-
-
-                 ;; [org.nd4j/nd4j-cuda-11.6-platform "1.0.0-M2.1"]
-
-                 ]
-  :source-paths ["src/clojure"]
-  :java-source-paths ["src/java"]
+                 [org.deeplearning4j/deeplearning4j-core "1.0.0-M2.1"]]
   :profiles {:dev {:source-paths ["repl"]}}
   :repl-options {:init-ns repl})
--- repl/repl.clj
+++ repl/repl.clj
@@ -3,9 +3,6 @@
             [clj-datavec.records [csv :as nscv]
                                  [jackson :as njack]]
             [clj-dl4j.core :as dl4j]
-            [clj-dl4j.examples [xor :as xor]
-                               [mnistanomaly :as mnist]
-                               [regression :as reg]]
             [clj-java-commons.core :refer :all]
             [clj-java-commons.coerce :refer [->clj]])
   (:refer-clojure :exclude [/]))
